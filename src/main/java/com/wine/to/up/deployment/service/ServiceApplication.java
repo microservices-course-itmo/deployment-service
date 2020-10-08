4d9f00ecb3181bbe46cf05f6f986aef42a1e4110
Arthasasyan <minearthas@gmail.com>
2020-10-08 13:00:16 +0300

    Initial commit

---
+++ src/main/java/com/wine/to/up/deployment/service/ServiceApplication.java
@@ -0,0 +1,18 @@
+package com.wine.to.up.deployment.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication
+@ComponentScan("com.wine.to.up")
+@EnableSwagger2
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}
