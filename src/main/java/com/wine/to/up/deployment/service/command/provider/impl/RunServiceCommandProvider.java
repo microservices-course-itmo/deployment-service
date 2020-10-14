c5427b3a7ddfddb67f2d7ba471650c7ec7eb2752
lolwhatvvw <vovkv231@gmail.com>
2020-10-14 19:12:14 +0300

    Add RunServiceCommandProvider

---
+++ src/main/java/com/wine/to/up/deployment/service/command/provider/impl/RunServiceCommandProvider.java
@@ -0,0 +1,38 @@
+package com.wine.to.up.deployment.service.command.provider.impl;
+
+import com.wine.to.up.deployment.service.command.processor.CommandProcessor;
+import com.wine.to.up.deployment.service.command.provider.ImageCommandProvider;
+import com.wine.to.up.deployment.service.dto.ImageDto;
+import com.wine.to.up.deployment.service.enums.ImageRequestType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Component("RunServiceCommandProvider")
+public class RunServiceCommandProvider implements ImageCommandProvider {
+
+    private CommandProcessor commandProcessor;
+    public static final String COMMAND = "docker service ls";
+
+    @Autowired
+    public void setCommandProcessor(final CommandProcessor commandProcessor) {
+        this.commandProcessor = commandProcessor;
+    }
+
+    @Override
+    public boolean accepts(ImageDto imageRequestDto) {
+        return imageRequestDto.getImageRequestType() == ImageRequestType.RUN;
+    }
+
+    @Override
+    public ImageDto process(ImageDto imageDto) {
+        final String answer = commandProcessor.processCommand(COMMAND);
+        final Map<String, Object> payload = new HashMap<>();
+        payload.put("answer", answer);
+        return ImageDto.builder().payload(Collections.unmodifiableMap(payload)).build();
+    }
+}
