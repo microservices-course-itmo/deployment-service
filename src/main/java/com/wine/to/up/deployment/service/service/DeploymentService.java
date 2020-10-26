0a27fe7ebaeb991f3e8d69ce616a2954804bc3a7
Arthasasyan <minearthas@gmail.com>
2020-10-26 22:04:15 +0300

    Initialize base architecture

--- src/main/java/com/wine/to/up/deployment/service/service/DeploymentService.java
+++ src/main/java/com/wine/to/up/deployment/service/service/DeploymentService.java
@@ -1,8 +1,6 @@
 package com.wine.to.up.deployment.service.service;
 
-import com.wine.to.up.deployment.service.dto.ImageDto;
-
 
 public interface DeploymentService {
-    ImageDto processImageRequest(final ImageDto imageDto);
+    void test();
 }
--- src/main/java/com/wine/to/up/deployment/service/service/impl/DeploymentServiceImpl.java
+++ src/main/java/com/wine/to/up/deployment/service/service/impl/DeploymentServiceImpl.java
@@ -1,30 +1,14 @@
 package com.wine.to.up.deployment.service.service.impl;
 
-import com.wine.to.up.deployment.service.command.provider.ImageCommandProvider;
-import com.wine.to.up.deployment.service.dto.ImageDto;
 import com.wine.to.up.deployment.service.service.DeploymentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 
 @Service
 public class DeploymentServiceImpl implements DeploymentService {
 
-    private List<ImageCommandProvider> imageCommandProviderList;
-
-    @Autowired
-    public void setImageCommandProviderList(final List<ImageCommandProvider> imageCommandProviderList) {
-        this.imageCommandProviderList = imageCommandProviderList;
-    }
-
     @Override
-    public ImageDto processImageRequest(final ImageDto imageDto) {
-        final var provider = imageCommandProviderList.stream()
-                .filter(it -> it.accepts(imageDto))
-                .findFirst()
-                .orElseThrow();
-        return provider.process(imageDto);
+    public void test() {
+        // DO something
     }
 }
