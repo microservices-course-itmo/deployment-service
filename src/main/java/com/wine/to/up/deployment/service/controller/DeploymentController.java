8878d222f4cfc5a326b5bbeb663a071390968bfb
lolwhatvvw <vovkv231@gmail.com>
2020-10-28 22:13:15 +0300

    Fixed merge conflicts

--- src/main/java/com/wine/to/up/deployment/service/controller/DeploymentController.java
+++ src/main/java/com/wine/to/up/deployment/service/controller/DeploymentController.java
@@ -1,9 +1,14 @@
 package com.wine.to.up.deployment.service.controller;
 
 import com.wine.to.up.deployment.service.service.DeploymentService;
+import com.wine.to.up.deployment.service.service.vo.ApplicationInstance;
+import com.wine.to.up.deployment.service.service.vo.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 @RestController
 public class DeploymentController {
@@ -14,4 +19,13 @@ public class DeploymentController {
     public void setDeploymentService(final DeploymentService deploymentService) {
         this.deploymentService = deploymentService;
     }
+
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/application-instances/{applicationName}")
+    public List<ApplicationInstance> appInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
 }
+
--- src/main/java/com/wine/to/up/deployment/service/service/impl/DeploymentServiceImpl.java
+++ src/main/java/com/wine/to/up/deployment/service/service/impl/DeploymentServiceImpl.java
@@ -1,7 +1,15 @@
 package com.wine.to.up.deployment.service.service.impl;
 
 import com.wine.to.up.deployment.service.service.DeploymentService;
+import com.wine.to.up.deployment.service.service.vo.ApplicationInstance;
+import com.wine.to.up.deployment.service.service.vo.DefaultApplicationInstance;
 import org.springframework.stereotype.Service;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 @Service
@@ -10,5 +18,25 @@ public class DeploymentServiceImpl implements DeploymentService {
     @Override
     public void test() {
         // DO something
+    }
+
+    @Override
+    public List<ApplicationInstance> getInstances(String serviceId) {
+        String hostname = "";
+        try {
+            List<ApplicationInstance> appInstances = new ArrayList<>();
+            InetAddress[] addresses = InetAddress.getAllByName(hostname);
+            if (addresses != null) {
+                for (InetAddress address : addresses) {
+                    DefaultApplicationInstance appInstance = new DefaultApplicationInstance(
+                            serviceId, address.getHostAddress(), 8080, false);
+                    appInstances.add(appInstance);
+                }
+            }
+            return appInstances;
+        }
+        catch (UnknownHostException e) {
+            return Collections.emptyList();
+        }
     }
 }
