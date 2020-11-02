61f5000b26b478c857aadcbaa2d44c6a61653cab
Matvej Baranov <baranov-matvej@mail.ru>
2020-11-02 12:13:55 +0300

    add mongoDB

--- src/main/java/com/wine/to/up/deployment/service/dao/ApplicationInstanceRepository.java
+++ src/main/java/com/wine/to/up/deployment/service/dao/ApplicationInstanceRepository.java
@@ -3,4 +3,9 @@ package com.wine.to.up.deployment.service.dao;
 import com.wine.to.up.deployment.service.entity.ApplicationInstance;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ApplicationInstanceRepository extends MongoRepository<ApplicationInstance, String> { }
+import java.util.List;
+
+public interface ApplicationInstanceRepository extends MongoRepository<ApplicationInstance, String>
+{
+    List<ApplicationInstance> findByTemplateId(Long templateId);
+}
