98e812de566a1887d4d32eb8ce6a6d06d6e89c18
Arthasasyan <minearthas@gmail.com>
2020-11-03 22:50:29 +0300

    Change plugins

--- src/main/java/com/wine/to/up/deployment/service/dao/ApplicationTemplateRepository.java
+++ src/main/java/com/wine/to/up/deployment/service/dao/ApplicationTemplateRepository.java
@@ -3,7 +3,5 @@ package com.wine.to.up.deployment.service.dao;
 import com.wine.to.up.deployment.service.entity.ApplicationTemplate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface ApplicationTemplateRepository extends MongoRepository<ApplicationTemplate, Long> {
 }
