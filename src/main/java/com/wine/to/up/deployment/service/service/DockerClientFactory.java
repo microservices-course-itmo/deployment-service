fff853d10df813fe87bcf203ce2a7f049f06958e
lolwhatvvw <vovkv231@gmail.com>
2020-11-16 21:27:29 +0500

    Added DockerSettings and edited DockerClientFactory

--- src/main/java/com/wine/to/up/deployment/service/service/DockerClientFactory.java
+++ src/main/java/com/wine/to/up/deployment/service/service/DockerClientFactory.java
@@ -1,20 +1,16 @@
 package com.wine.to.up.deployment.service.service;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.wine.to.up.deployment.service.entity.DockerSettings;
-import com.wine.to.up.deployment.service.vo.ApplicationTemplateVO;
-import com.wine.to.up.deployment.service.vo.DockerSettingsVO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DockerClientFactory {
 
-    public DockerSettingsVO getDockerClient(String dockerAddress) {
-        return DockerSettingsVO.builder()
-                .id("1")
-                .dockerAddress(dockerAddress)
-                .registry("1")
+    public DockerClient getDockerClient(DockerSettings dockerSettings ) {
+        return (DockerClient) DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost(dockerSettings.getDockerAddress())
                 .build();
     }
 }
