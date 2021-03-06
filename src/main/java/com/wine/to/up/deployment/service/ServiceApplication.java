06c969dbdac6a80c0106e69afdd73c9b4c5078b5
Arthasasyan <minearthas@gmail.com>
2021-03-06 20:10:09 +0300

    Add Eureka configs

--- src/main/java/com/wine/to/up/deployment/service/ServiceApplication.java
+++ src/main/java/com/wine/to/up/deployment/service/ServiceApplication.java
@@ -2,6 +2,7 @@ package com.wine.to.up.deployment.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -9,6 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @ComponentScan("com.wine.to.up")
 @EnableSwagger2
+@EnableDiscoveryClient
 public class ServiceApplication {
 
     public static void main(String[] args) {
