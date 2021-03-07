6df2de457d9194b197660a46228b5b0d1a427d2c
ilyashn <ilyashin9@gmail.com>
2021-03-07 18:56:08 +0300

    remove auth for tests

--- src/main/java/com/wine/to/up/deployment/service/controller/DeploymentController.java
+++ src/main/java/com/wine/to/up/deployment/service/controller/DeploymentController.java
@@ -20,7 +20,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-@PreAuthorize("isAuthenticated()")
+//@PreAuthorize("isAuthenticated()")
 public class DeploymentController {
 
     private DeploymentService deploymentService;
