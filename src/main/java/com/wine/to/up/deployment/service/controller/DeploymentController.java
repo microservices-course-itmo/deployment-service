dae1d2174d3f55ccd9f63fac7ac830d00df636eb
Alexander Kosturenko <akosturenko@solanteq.com>
2021-03-08 22:20:32 +0300

    Remove authorization

--- src/main/java/com/wine/to/up/deployment/service/controller/DeploymentController.java
+++ src/main/java/com/wine/to/up/deployment/service/controller/DeploymentController.java
@@ -9,7 +9,6 @@ import com.wine.to.up.deployment.service.vo.ApplicationTemplateVO;
 import com.wine.to.up.deployment.service.vo.SettingsVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.NotFoundException;
@@ -17,7 +16,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-@PreAuthorize("isAuthenticated()")
+//@PreAuthorize("isAuthenticated()")
 public class DeploymentController {
 
     private DeploymentService deploymentService;
